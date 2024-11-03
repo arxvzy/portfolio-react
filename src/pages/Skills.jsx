8c9ded690136521789d8e818ea86f9756ea30696
Arxvzy <szaryabima@gmail.com>
2024-11-03 14:27:07 +0700

    feat: add skills page

---
+++ src/pages/Skills.jsx
@@ -0,0 +1,91 @@
+import { Helmet } from "react-helmet";
+import htmlImage from "../assets/html.svg";
+import cssImage from "../assets/css.svg";
+import jsImage from "../assets/js.svg";
+import tailwindImage from "../assets/tailwind.svg";
+import shadcnImage from "../assets/shadcn.svg";
+import viteImage from "../assets/vite.svg";
+import reactImage from "../assets/react.svg";
+import nodeImage from "../assets/node.svg";
+import expressImage from "../assets/express.svg";
+import mysqlImage from "../assets/mysql.svg";
+import mongoImage from "../assets/mongo.svg";
+import prismaImage from "../assets/prisma.png";
+import phpImage from "../assets/php.svg";
+import laravelImage from "../assets/laravel.svg";
+
+import SkillsList from "../components/SkillsList";
+import SkillCard from "../components/SkillCard";
+
+const frontEndSkills = [
+  {
+    name: "HTML",
+    image: htmlImage,
+  },
+  {
+    name: "CSS",
+    image: cssImage,
+  },
+  {
+    name: "JS",
+    image: jsImage,
+  },
+  {
+    name: "Tailwind",
+    image: tailwindImage,
+  },
+  {
+    name: "Vite",
+    image: viteImage,
+  },
+  {
+    name: "React",
+    image: reactImage,
+  },
+  {
+    name: "Shadcn/ ui",
+    image: shadcnImage,
+  },
+];
+
+const backEndSkills = [
+  {
+    name: "Node JS",
+    image: nodeImage,
+  },
+  {
+    name: "Express JS",
+    image: expressImage,
+  },
+  {
+    name: "MySQL",
+    image: mysqlImage,
+  },
+  {
+    name: "MongoDB",
+    image: mongoImage,
+  },
+  {
+    name: "Prisma ORM",
+    image: prismaImage,
+  },
+  {
+    name: "PHP",
+    image: phpImage,
+  },
+  {
+    name: "Laravel",
+    image: laravelImage,
+  },
+];
+export default function Skills() {
+  return (
+    <div className="flex flex-col w-full pb-10">
+      <Helmet>
+        <title>Skills | Arya Bima</title>
+      </Helmet>
+      <SkillsList name="Front End" skills={frontEndSkills} />
+      <SkillsList name="Back End" skills={backEndSkills} />
+    </div>
+  );
+}
